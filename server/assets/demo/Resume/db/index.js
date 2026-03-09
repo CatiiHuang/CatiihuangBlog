e57d1cc36a199e331a0dc03c155c3c5ee5fcc857
huangguoqing <huangguoqing.hgq@bytedance.com>
2026-03-09 19:44:39 +0800

    feat:添加服务代码

---
+++ server/assets/demo/Resume/db/index.js
@@ -0,0 +1,13 @@
+module.exports = () => {
+  const mongoose = require("mongoose");
+  mongoose.connect(
+    "mongodb://catii:gqgq123123@127.0.0.1:27017/tao?authSource=admin",
+    // "mongodb://127.0.0.1:27017/grjl?authSource=admin",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  );
+
+  // 为这次连接绑定事件
+  const db = mongoose.connection;
+  db.once("error", () => console.log("Mongo connection error"));
+  db.once("open", () => console.log("Mongo connection successed"));
+};
---
+++ server/index.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const api = require("./api");
+const app = express();
+// const cookieParser=require('cookie-parser');
+const session = require("express-session");
+
+//跨域
+app.use(cors());
+// app.use(cookieParser());
+app.use(express.json({ limit: "5mb" }));
+// app.use(session({
+//     secret:'12345',
+//     cookie:{maxAge:60 * 1000 * 60},
+//     resave:false,
+//     saveUninitialized:trueA
+// }))
+
+app.use(bodyParser.json());
+//当extended为false的时候，键值对中的值就为'String'或'Array'形式，为true的时候，则可为任何数据类型。
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(api);
+// //配置静态资源库 在8000端口下
+
+app.use("/assets", express.static(path.join(__dirname, "./assets"))); //配置静态资源库
+app.use(express.static(path.join(__dirname, "./demo"))); //配置静态资源库
+// 监听8000端口
+app.listen(8088);
+console.log("success listen…………");
